3b7f9401360a693d79b3b56dea248c39c88ad8ce
Matthias Springer <me@matthiasspringer.de>
2014-01-18 21:04:11 -0800

    Vectorized do_block and buffered A.

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -38,6 +38,15 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
     }
 }
 
+// #define INNER_DOBLOCK_LOOP 1
+
+#define MU 4
+#define NU 2
+#define KU 4
+
+#define BUFFER_A 1
+
+double* A_buffered;
 double* B_transposed;
 static void do_block_unrolled (int lda, int BLOCK_SIZE_REGISTER, double* A, double* B, double* C)
 {
@@ -49,44 +58,124 @@ static void do_block_unrolled (int lda, int BLOCK_SIZE_REGISTER, double* A, doub
 			B_transposed[BLOCK_SIZE_REGISTER*j+k] = B[k*lda+j];
 		}
 	}
+
+#ifdef BUFFER_A
+	// buffer matrix A
+	for (int i = 0; i < BLOCK_SIZE_REGISTER; i++) {
+		for (int k = 0; k < BLOCK_SIZE_REGISTER; k++) {
+			A_buffered[i*BLOCK_SIZE_REGISTER + k] = A[i*lda + k];
+		}
+	}
+#endif
+
+	double c_d_1;
+	double c_d_2;
 	
   /* For each row i of A */
-  for (int i = 0; i < BLOCK_SIZE_REGISTER; ++i)
+#ifdef INNER_DOBLOCK_LOOP
+  for (int i = 0; i < BLOCK_SIZE_REGISTER; i += MU)
+#endif
+#ifndef INNER_DOBLOCK_LOOP
+	for (int i = 0; i < BLOCK_SIZE_REGISTER; ++i)
+#endif
     /* For each column j of B */
-    for (int j = 0; j < BLOCK_SIZE_REGISTER; ++j)
+#ifdef INNER_DOBLOCK_LOOP
+    for (int j = 0; j < BLOCK_SIZE_REGISTER; j += NU)
+#endif
+#ifndef INNER_DOBLOCK_LOOP
+		for (int j = 0; j < BLOCK_SIZE_REGISTER; ++j)
+#endif
     {
       /* Compute C(i,j) */
+#ifndef INNER_DOBLOCK_LOOP
       double cij = C[i*lda+j];
-      for (int k = 0; k < BLOCK_SIZE_REGISTER; k += 4) {
-				int index_A = i*lda + k;
+#endif
+
+#ifdef INNER_DOBLOCK_LOOP
+      for (int k = 0; k < BLOCK_SIZE_REGISTER; k += KU) {
+#endif
+#ifndef INNER_DOBLOCK_LOOP
+			for (int k = 0; k < BLOCK_SIZE_REGISTER; k += 8) {
+#endif
+#ifdef INNER_DOBLOCK_LOOP
+
+				for (int k00 = k; k00 < k+KU; k00++) {
+							int index_A = i*BLOCK_SIZE_REGISTER + k;
+							int index_B = j*BLOCK_SIZE_REGISTER + k;
+
+							//     cij
+							double c00 = A_buffered[index_A] * B_transposed[index_B];
+							double c01 = A_buffered[index_A] * B_transposed[index_B + BLOCK_SIZE_REGISTER];
+							double c10 = A_buffered[index_A + BLOCK_SIZE_REGISTER] * B_transposed[index_B];
+							double c11 = A_buffered[index_A + BLOCK_SIZE_REGISTER] * B_transposed[index_B + BLOCK_SIZE_REGISTER];
+							double c20 = A_buffered[index_A + 2 * BLOCK_SIZE_REGISTER] * B_transposed[index_B];
+							double c21 = A_buffered[index_A + 2 * BLOCK_SIZE_REGISTER] * B_transposed[index_B + BLOCK_SIZE_REGISTER];
+							double c30 = A_buffered[index_A + 3 * BLOCK_SIZE_REGISTER] * B_transposed[index_B];
+							double c31 = A_buffered[index_A + 3 * BLOCK_SIZE_REGISTER] * B_transposed[index_B + BLOCK_SIZE_REGISTER];
+
+							C[i*lda + j] += c00;
+							C[i*lda + j+1] += c01;
+							C[(i+1)*lda + j] += c10;
+							C[(i+1)*lda + j+1] += c11;
+							C[(i+2)*lda + j] += c20;
+							C[(i+2)*lda + j+1] += c21;
+							C[(i+3)*lda + j] += c30;
+							C[(i+3)*lda + j+1] += c31;
+
+//					for (int j00 = j; j00 < j + NU; j00++) {			// 2
+//						for (int i00 = i; i00 < i + MU; i00++) {		// 4
+//							C[i00*lda + j00] += A[i00*lda + k00] * B_transposed[j00*BLOCK_SIZE_REGISTER + k00];
+//						}
+//					}
+				}
+#endif
+
+#ifndef INNER_DOBLOCK_LOOP
+				int index_A = i*BLOCK_SIZE_REGISTER + k;
 				int index_B = j*BLOCK_SIZE_REGISTER + k;
 			
-				__m128d a1 = _mm_loadu_pd(A + index_A);
-				__m128d a2 = _mm_loadu_pd(A + index_A + 2);
-        __m128d b1 = _mm_loadu_pd(B + index_B);
-        __m128d b2 = _mm_loadu_pd(B + index_B + 2);
+				__m128d a1 = _mm_load_pd(A_buffered + index_A);
+				__m128d a2 = _mm_load_pd(A_buffered + index_A + 2);
+        __m128d a3 = _mm_load_pd(A_buffered + index_A + 4);
+        __m128d a4 = _mm_load_pd(A_buffered + index_A + 6);
+
+        __m128d b1 = _mm_load_pd(B_transposed + index_B);
+        __m128d b2 = _mm_load_pd(B_transposed + index_B + 2);
+        __m128d b3 = _mm_load_pd(B_transposed + index_B + 4);
+        __m128d b4 = _mm_load_pd(B_transposed + index_B + 6);
+
+				__m128d d1 = _mm_mul_pd(a1, b1);
+				__m128d d2 = _mm_mul_pd(a2, b2);
+        __m128d d3 = _mm_mul_pd(a3, b3);
+        __m128d d4 = _mm_mul_pd(a4, b4);
+
+				__m128d c1 = _mm_add_pd(d1, d2);
+				__m128d c2 = _mm_add_pd(d3, d4);
+				__m128d c3 = _mm_add_pd(c1, c2);
 				
-				//__m128d d1 = _mm_mul_pd(a1, b1);
-				//__m128d d2 = _mm_mul_pd(a2, b2);
-
-				__m128d c = _mm_add_pd(_mm_mul_pd(a1, b1), _mm_mul_pd(a2, b2));
-
-				double c1;
-				double c2;
-
-				_mm_storeh_pd(&c1, c);
-				_mm_storel_pd(&c2, c);
-				
-				//double cij1 = A[index_A] * B_transposed[index_B];
-				//double cij2 = A[index_A + 1] * B_transposed[index_B + 1];
-				//double cij3 = A[index_A + 2] * B_transposed[index_B + 2];
-				//double cij4 = A[index_A + 3] * B_transposed[index_B + 3];
+/*
+				double cij1 = A[index_A] * B_transposed[index_B];
+			  double cij2 = A[index_A + 1] * B_transposed[index_B + 1];
+				double cij3 = A[index_A + 2] * B_transposed[index_B + 2];
+				double cij4 = A[index_A + 3] * B_transposed[index_B + 3];
 				//double cres = 
-			  //cij += cij1 + cij2 + cij3 + cij4;	 //+ cij5 + cij6; // cij6 + cij7 + cij8;
-			  cij += c1 + c2;
+
+			  cij += cij1 + cij2 + cij3 + cij4;	 //+ cij5 + cij6; // cij6 + cij7 + cij8;
+//			  cij += c_d_1 + c_d_2;
+*/
+
+					cij += _mm_cvtsd_f64(c3);
+					__m128d c4 = _mm_unpackhi_pd(c3, c1);
+					cij += _mm_cvtsd_f64(c4);
+
+#endif
+
 			}
 
+#ifndef INNER_DOBLOCK_LOOP
       C[i*lda+j] = cij;
+#endif
+
     }
 }
 
@@ -101,6 +190,7 @@ void square_dgemm (int lda, double* A, double* B, double* C)
 	int fringe_start = lda / BLOCK_SIZE_REGISTER * BLOCK_SIZE_REGISTER;
 
 	B_transposed = (double*) memalign(16, sizeof(double) * BLOCK_SIZE_REGISTER*BLOCK_SIZE_REGISTER);
+	A_buffered = (double*) memalign(16, sizeof(double) * BLOCK_SIZE_REGISTER*BLOCK_SIZE_REGISTER);
 
   /* For each block-row of A */ 
   for (int i = 0; i < fringe_start; i += BLOCK_SIZE_REGISTER)
